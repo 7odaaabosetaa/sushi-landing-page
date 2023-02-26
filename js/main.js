3acf802170b5e8b1c44dbff816a2bccc0b11f6cc
mahmoud gomaa <112572054+7odaaabosetaa@users.noreply.github.com>
2023-02-26 23:37:40 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let i = document.querySelector("i");
+let ul = document.querySelector("header ul");
+let body = document.querySelector("body");
+
+i.addEventListener("click",()=>{
+    ul.classList.toggle("active");
+})
